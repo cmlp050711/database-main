7e2bfd48f7164db09ca8b2e16558f42481e60056
cmlp <321231035@qq.com>
2023-01-10 11:21:57 +0800

    登录

--- src/components/common/login.vue
+++ src/components/common/login.vue
@@ -6,7 +6,7 @@
       <el-col :lg="8" :xs="16" :md="10" :span="10">
         <div class="top" >
           <img src="static/picture/logo.png" alt="img" style="height: 50px; width: 70px; padding-top: 2vh">
-          <span class="title">&nbsp;&nbsp;在线学习平台</span>
+          <span class="title">&nbsp;&nbsp;登陆</span>
         </div>
         <div class="bottom">
           <div class="container">
@@ -29,6 +29,27 @@
 <!--                </div>-->
               </div>
             </el-form>
+
+<!--            选择角色-->
+
+            <el-dialog title="请选择角色" :visible.sync="dialogFormVisible" style="width: 60vw;margin: 0 auto">
+              <el-form :model="form">
+                <el-form-item label="选择角色" :label-width="formLabelWidth">
+                  <el-select v-model="form.role" placeholder="请选择">
+                    <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary"  @click="clickRole(form.role)">确 定</el-button>
+              </div>
+            </el-dialog>
           </div>
         </div>
       </el-col>
@@ -50,61 +71,137 @@ export default {
   name: "login",
   data() {
     return {
-      role: 2,
+      resData:{},
+      form:{
+        role:""
+      },
+      dialogFormVisible: false,
+      formLabelWidth:'120px',
+      options:[],
       labelPosition: 'left',
       formLabelAlign: {
       }
     }
   },
   methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+    },
     //用户登录请求后台处理
     login() {
       console.log("登录操作执行-------");
+      console.log(this.formLabelAlign)
       this.$axios({
-        url: `/api/user/login`,
+        url: `http://localhost:8080/user/login`,
         method: 'post',
         data: {
           ...this.formLabelAlign
         }
       }).then(res=>{
-        let resData = res.data.data
-        if(resData != null) {
-          switch(resData.roleSet) {
-            case "sys_admin":  //管理员
-              this.$cookies.set("aname", resData.Name)
-              this.$cookies.set("aid", resData.id)
-              // this.$cookies.set("role", 0)
-              this.$router.push({path: '/sysindex' }) //跳转到首页
-              break
-            case "course_admin": //课程管理员
-              this.$cookies.set("cname", resData.Name)
-              this.$cookies.set("cid", resData.id)
-              // this.$cookies.set("role", 1)
-              this.$router.push({path: '/index' }) //跳转到教师用户
-              break
-            case "user": //学生
-              this.$cookies.set("sname", resData.Name)
-              this.$cookies.set("sid", resData.id)
-              this.$router.push({path: '/home'})
-              break
-            case "leaders": //领导
-              this.$cookies.set("lname", resData.Name)
-              this.$cookies.set("lid", resData.id)
-              this.$router.push({path: '/leaderindex'})
-              break
+        this.resData = res.data
+        console.log(this.resData)
+        if(this.resData.res.code === 200) {
+          this.$cookies.set("token", this.resData.token)
+          let roles=this.resData.user.roleSet
+          console.log(roles)
+          if (roles.length===1) {
+            switch (roles[0].name) {
+              case "sys_admin":  //管理员
+                this.$cookies.set("aname", this.resData.Name)
+                this.$cookies.set("aid", this.resData.id)
+                // this.$cookies.set("role", 0)
+                this.$router.push({path: '/sysindex'}) //跳转到首页
+                break
+              case "course_admin": //课程管理员
+                this.$cookies.set("cname", this.resData.Name)
+                this.$cookies.set("cid", this.resData.id)
+                // this.$cookies.set("role", 1)
+                this.$router.push({path: '/index'}) //跳转到教师用户
+                break
+              case "member": //学生
+                this.$cookies.set("sname", this.resData.Name)
+                this.$cookies.set("sid", this.resData.id)
+                this.$router.push({path: '/home'})
+                break
+              case "leaders": //领导
+                this.$cookies.set("lname", this.resData.Name)
+                this.$cookies.set("lid", this.resData.id)
+                this.$router.push({path: '/leaderindex'})
+                break
+            }
+          }
+          else{
+            roles.forEach((item, index, array) => {
+              console.log(item);
+              let label=this.addName(item.name)
+              let value=item.name
+              this.options.push({label: label,value: value});
+            });
+            console.log("【options】"+this.options)
+            this.$message({
+              showClose: true,
+              type: 'warning',
+              message: '您有多个角色，请选择登录角色'
+            })
+            this.dialogFormVisible=true
           }
         }
-        if(resData == null) { //错误提示
+        else { //错误提示
           this.$message({
             showClose: true,
             type: 'error',
-            message: '用户名或者密码错误'
+            message: this.resData.res.msg
           })
         }
       })
     },
     clickTag(key) {
       this.role = key
+    },
+    clickRole(roles){
+      this.dialogFormVisible = false
+      this.options={}
+      switch (roles) {
+        case "sys_admin":  //管理员
+          this.$cookies.set("aname", this.resData.Name)
+          this.$cookies.set("aid", this.resData.id)
+          // this.$cookies.set("role", 0)
+          this.$router.push({path: '/sysindex'}) //跳转到首页
+          break
+        case "course_admin": //课程管理员
+          this.$cookies.set("cname", this.resData.Name)
+          this.$cookies.set("cid", this.resData.id)
+          // this.$cookies.set("role", 1)
+          this.$router.push({path: '/index'}) //跳转到教师用户
+          break
+        case "member": //学生
+          this.$cookies.set("sname", this.resData.Name)
+          this.$cookies.set("sid", this.resData.id)
+          this.$router.push({path: '/home'})
+          break
+        case "leaders": //领导
+          this.$cookies.set("lname", this.resData.Name)
+          this.$cookies.set("lid", this.resData.id)
+          this.$router.push({path: '/leaderindex'})
+          break
+      }
+
+    },
+    addName(roles){
+      switch (roles) {
+        case "sys_admin":  //管理员
+          return "系统管理员"
+        case "course_admin": //课程管理员
+          return "课程管理员"
+        case "member": //学生
+          return "会员"
+        case "leader": //领导
+          return "公司领导"
+      }
     }
   },
   computed: mapState(["userInfo"]),
